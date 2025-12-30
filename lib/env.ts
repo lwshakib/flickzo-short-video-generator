a8c4789642370652f589b55b6edad52fba9a5294
lwshakib <leadwithshakib@gmail.com>
2025-12-30 09:44:34 +0600

    feat: Add environment variable exports for authentication, Google services, Cloudinary, and database configuration.

---
+++ lib/env.ts
@@ -0,0 +1,19 @@
+// auth
+export const BETTER_AUTH_SECRET = process.env.BETTER_AUTH_SECRET;
+export const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const BETTER_AUTH_URL = process.env.BETTER_AUTH_URL;
+
+// google oauth
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+// google api keys (comma-separated)
+export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+
+// cloudinary
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+
+// database (example you mentioned)
+export const DATABASE_URL = process.env.DATABASE_URL;
